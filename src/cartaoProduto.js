359d356592a5a3f2cffbe7bc001098bb5a51931a
lucas-bavaresco <l.bavaresco.pereira@gmail.com>
2023-09-03 00:42:29 -0300

    Primeiro commit do Projeto de Intensivao de JavaScript da Hashtag

---
+++ src/cartaoProduto.js
@@ -0,0 +1,28 @@
+import { adicionarAoCarrinho } from "./menuCarrinho";
+import { catalogo } from "./utilidades";
+
+export function renderizarCatalogo() {
+  for (const produtoCatalogo of catalogo) {
+    const cartaoProduto = `<div class='border-solid shadow-xl shadow-slate-400 rounded-lg g w-48 m-2 flex flex-col pb-2 justify-between group ${
+      produtoCatalogo.feminino ? "feminino" : "masculino"
+    }' id="card-produto-${produtoCatalogo.id}">
+      <img src="./assets/img/${
+        produtoCatalogo.imagem
+      }" alt="Produto 1 do Ecommerce" class="group-hover:scale-110 duration-300 m-3 rounded-lg" />
+      <p class='marca'>${produtoCatalogo.marca}</p>
+      <p class='text-sm'>${produtoCatalogo.nome}</p>
+      <p class='text-sm'>R$ ${produtoCatalogo.preco}</p>
+      <button id='adicionar-${
+        produtoCatalogo.id
+      }' class='bg-slate-950 hover:bg-slate-700 text-slate-200'><i class="fa-solid fa-cart-plus"></i></button>
+      </div>`;
+
+    document.getElementById("container-produto").innerHTML += cartaoProduto;
+  }
+
+  for (const produtoCatalogo of catalogo) {
+    document
+      .getElementById(`adicionar-${produtoCatalogo.id}`)
+      .addEventListener("click", () => adicionarAoCarrinho(produtoCatalogo.id));
+  }
+}
---
+++ src/utilidades.js
@@ -0,0 +1,120 @@
+export const catalogo = [
+  {
+    id: "1",
+    nome: "Camisa Larga com Bolsos",
+    marca: "Zara",
+    preco: 70.0,
+    imagem: "product-1.jpg",
+    feminino: false,
+  },
+  {
+    id: "2",
+    nome: "Casaco Reto com Lã",
+    marca: "Zara",
+    preco: 85,
+    imagem: "product-2.jpg",
+    feminino: true,
+  },
+  {
+    id: "3",
+    nome: "Jaqueta com Efeito Camurça",
+    marca: "Zara",
+    preco: 60,
+    imagem: "product-3.jpg",
+    feminino: false,
+  },
+  {
+    id: "4",
+    nome: "Sobretudo em Mescla de Lã",
+    marca: "Zara",
+    preco: 160,
+    imagem: "product-4.jpg",
+    feminino: false,
+  },
+  {
+    id: "5",
+    nome: "Camisa Larga Acolchoada de Veludo Coletê",
+    marca: "Zara",
+    preco: 110,
+    imagem: "product-5.jpg",
+    feminino: false,
+  },
+  {
+    id: "6",
+    nome: "Casaco de Lã com Botões",
+    marca: "Zara",
+    preco: 170,
+    imagem: "product-6.jpg",
+    feminino: true,
+  },
+  {
+    id: "7",
+    nome: "Casaco com Botões",
+    marca: "Zara",
+    preco: 75,
+    imagem: "product-7.jpg",
+    feminino: true,
+  },
+  {
+    id: "8",
+    nome: "Colete Comprido com Cinto",
+    marca: "Zara",
+    preco: 88.0,
+    imagem: "product-8.jpg",
+    feminino: true,
+  },
+];
+
+export function salvarLocalStorage(chave, informacao) {
+  localStorage.setItem(chave, JSON.stringify(informacao));
+}
+export function lerLocalStorage(chave) {
+  return JSON.parse(localStorage.getItem(chave));
+}
+
+export function apagarDoLocalStorage(chave) {
+  localStorage.removeItem(chave);
+}
+
+export function desenharProdutoCarrinhoSimples(
+  idProduto,
+  idContainerHtml,
+  quantidadeProduto
+) {
+  const produto = catalogo.find((p) => p.id === idProduto); //encontra na lista de Produtos o mesmo ID solicitado na entrada desta função
+  const containerProdutosCarrinho = document.getElementById(idContainerHtml);
+
+  const elementoArticle = document.createElement("article");
+  const articleClasses = [
+    "flex",
+    "bg-stone-200",
+    "rounded-lg",
+    "p-1",
+    "relative",
+    "mb-2",
+    "w-96",
+  ];
+
+  for (const articleClass of articleClasses) {
+    elementoArticle.classList.add(articleClass);
+  }
+
+  const cartaoProdutoCarrinho = `
+        <img src="./assets/img/${produto.imagem}" alt="Carrinho: ${
+    produto.nome
+  }" class="h-24 rounded-lg" />
+    <div class="p-2 flex flex-col justify-between">
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
+      <p class="text-slate-400 text-xs">Tamanho: M</p>
+      <p class="text-green-700 text-lg">R$ ${produto.preco}</p>
+    </div>
+    <div class="flex text-slate-950 items-end absolute bottom-0 right-2 text-lg">
+      <p id="quantidade-${produto.id}" class="ml-2">${
+    quantidadeProduto[produto.id]
+  } </p>
+    </div>
+  `;
+
+  elementoArticle.innerHTML = cartaoProdutoCarrinho;
+  containerProdutosCarrinho.appendChild(elementoArticle);
+}
